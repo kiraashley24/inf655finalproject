14664f66da3913ddc6afb393933a55c84412f44c
Kira Selucky <kiraashley24@gmail.com>
2024-05-05 13:28:03 -0600

    all products

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home";
 import PageNotFound from "./pages/404/PageNotFound";
 import ProductInfo from "./pages/productInfo/ProductInfo";
 import CartPage from "./pages/cart/CartPage";
+import AllProducts from "./pages/allProducts/AllProducts";
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/*" element={<PageNotFound />} />
         <Route path="/productinfo" element={<ProductInfo />} />
+        <Route path="/allProducts" element={<AllProducts />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
       </Router>
