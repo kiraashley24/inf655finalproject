aa2f84665faa3ea1b3d96dda396ba17d6a05562d
Kira Selucky <kiraashley24@gmail.com>
2024-05-05 12:51:05 -0600

    created reviews and added to homepage

--- src/components/homePageProducts/HomePageProducts.jsx
+++ src/components/homePageProducts/HomePageProducts.jsx
@@ -1,21 +1,57 @@
+import { useNavigate } from "react-router-dom";
 import React from 'react'
+import homeData from "./data";
 
-const HomePageProducts = () => {
+const HomePageProductCard = () => {
+const navigate = useNavigate();
+
   return (
     <div className="mt-10">
-        <div className='bh-line-100'>
-            <h1 className='text-center mb-5 text-2x bg-yellow-900 font-semibold'>
-                Popular Items
-            </h1>
+      <div className="bg-amber-600 p-5">
+        <h1 className="text-center mb-5 text-2xl font-semibold">
+          Best Selling Products
+        </h1>
+      </div>
+      <section className="text-gray-600 body-font bg-teal-900">
+        <div className="container px-5 py-5 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            {homeData.map((item, index) => {
+              const { image, name, price } = item;
+              return (
+                <div key={index} className="p-4 w-full md:w-1/4">
+                  <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
+                    <img
+                      className="lg:h-80 h-96 w-full"
+                      onClick={() =>
+                        navigate("/productInfo", { state: { product: item } })
+                      }
+                      src={image}
+                      alt="img"
+                    />
+                    <div className="p-6 bg-orange-100">
+                      <h1 className="name-font text-lg font-medium text-gray-900 mb-3">
+                        {name.substring(0, 25)}
+                      </h1>
+                      <h1 className="name-font text-lg font-medium text-gray-900 mb-3">
+                        ${price}
+                      </h1>
+                      <div className="flex justify-center">
+                        <button
+                          className=" bg-amber-500 hover:bg-yellow-600 w-full text-black py-[4px] rounded-lg font-bold"
+                          
+                        >
+                          Add to Cart
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <section className='text-gray-600 body-font bg-teal-900'>
-            <div className='container px-5 py-5 mx-auto'>
-                <div className='flex flex-wrap m-4'></div>
-
-            </div>
-        </section>
+      </section>
     </div>
-  )
-}
-
-export default HomePageProducts;
+  );
+};
+export default HomePageProductCard;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '../../components/layout/Layout';
 import HeroSection from '../../components/heroSection/HeroSection';
 import Category from "../../components/category/Category.jsx";
+import HomePageProductCard from '../../components/homePageProducts/HomePageProducts.jsx';
+import Review from '../../components/reviews/Reviews.jsx';
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
     <Layout>
       <HeroSection />
       <Category />
+      < HomePageProductCard/>
+      < Review/>
     </Layout>
     </>
   );
