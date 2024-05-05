2c7f47ecc9ec0a04003717c6dcc3476effc0cb63
Kira Selucky <kiraashley24@gmail.com>
2024-05-05 13:03:10 -0600

    product page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from "./pages/home/Home";
 import PageNotFound from "./pages/404/PageNotFound";
+import ProductInfo from "./pages/productInfo/ProductInfo";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/*" element={<PageNotFound />} />
+        <Route path="/productinfo" element={<ProductInfo />} />
       </Routes>
       </Router>
     </>
